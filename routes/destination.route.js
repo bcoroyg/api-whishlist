88a0f9da6ff09bd7317e1c37b2c43b21d8639dc4
Brandon Coroy <becgonzalez25@gmail.com>
2022-05-23 17:59:42 -0600

    Creacion de ruta de buscador

--- routes/destination.route.js
+++ routes/destination.route.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import { 
     getDestinations, 
-    postDestination 
+    postDestination, 
+    searchDestination,
+    getTranslation
 } from '../controllers/ destination.controller.js';
 
 const router = express.Router();
 
-router.get('/',  getDestinations);
-router.post('/',  postDestination);
+router.get('/', getDestinations);
+router.get('/search', searchDestination);
+router.post('/', postDestination);
+router.get('/translation', getTranslation);
 
 export default router;
